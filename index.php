1a873f3ce59097772c3839b70bb595caf510b886
Khaled <makkawirelang@gmail.com>
2023-08-31 16:08:39 +0700

    update favicon to main

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
   <meta name="author" content="MAA" />
 
   <!-- Favicon -->
-  <link rel="shortcut icon" href="img/demos/business-consulting-3/logo-MAA-component.png" type="image/x-icon" />
+  <link rel="shortcut icon" href="img/demos/business-consulting-3/favicon.png" type="image/x-icon" />
   <link rel="apple-touch-icon" href="img/apple-touch-icon.png" />
   <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
 
